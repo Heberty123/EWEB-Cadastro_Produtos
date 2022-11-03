e574edd07704d88ba9ee13ceab616933327178c5
Heberty <hebertyprado22005@gmail.com>
2022-11-03 18:57:15 -0300

    interface mock produtos was added

---
+++ src/app/mock-produto.ts
@@ -0,0 +1,14 @@
+import { Produto } from "./produto";
+
+export const Produtos: Produto[] = [
+    { id: 1, name: 'Celular', preco: 1240.50, codigo: "348574", cor: "vermelho" },
+    { id: 2, name: 'TV', preco: 4420.78, codigo: "348168", cor: "preto" },
+    { id: 3, name: 'liquidificador', preco: 245.00, codigo: "876346", cor: "vermelho" },
+    { id: 4, name: 'tablet', preco: 2660.80, codigo: "765114", cor: "branco" },
+    { id: 5, name: 'notebook', preco: 5200.50, codigo: "762246", cor: "preto" },
+    { id: 6, name: 'impressora', preco: 760.70, codigo: "236557", cor: "cinza" },
+    { id: 7, name: 'geladeira', preco: 3233.20, codigo: "543678", cor: "preto" },
+    { id: 8, name: 'monitor', preco: 1315.99, codigo: "445635", cor: "preto" },
+    { id: 9, name: 'desktop', preco: 7664.89, codigo: "143865", cor: "azul" },
+    { id: 10, name: 'console xbox', preco: 6630.75, codigo: "765456", cor: "cinza" }
+]
