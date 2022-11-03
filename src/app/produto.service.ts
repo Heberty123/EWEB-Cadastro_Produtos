a14ec500fc167e0475d8fa84e0be89a9db9928db
Heberty <hebertyprado22005@gmail.com>
2022-11-03 19:08:14 -0300

    the service that offers all products was created

--- src/app/produto.service.ts
+++ src/app/produto.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Produto } from './produto';
+import { Produtos } from './mock-produto';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class ProdutoService {
 
   constructor() { }
+
+  getProdutos(): Observable<Produto[]> {
+    const produtos = of(Produtos);
+    return produtos;
+  } 
 }
